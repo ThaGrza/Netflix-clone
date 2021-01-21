4eb3af68cb732d22eb30a1c104f02c1075da0321
ThaGrza <andrewgriswold7@gmail.com>
2021-01-21 18:45:43 -0500

    row component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import requests from './requests';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello This is a netflix clone app</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}  />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}  />
+
     </div>
   )
 };
---
+++ src/Row.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react'
+import axios from './axios';
+
+function Row({ title, fetchUrl}) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData(){
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  
+  console.log(movies);  
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="rowPosters">
+        {/* several poster rows */}
+
+        {movies.map(movies)}
+      </div>
+    </div>
+  )
+}
+
+export default Row;
