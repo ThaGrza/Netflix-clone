217ac5cf012b68ce8d57aae81c576c7720abbc74
ThaGrza <andrewgriswold7@gmail.com>
2021-02-09 17:55:37 -0500

    hiding api key

--- src/requests.js
+++ src/requests.js
@@ -1,14 +1,14 @@
- const API_KEY = "5f9630b664fee3f1c639e0ae94090867";
+import REACT_API_KEY from 'react-dotenv';
 
  const requests = {
-   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
-   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-   fetchDocumentariesMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`
+   fetchTrending: `/trending/all/week?api_key=${REACT_API_KEY}&language=en-us`,
+   fetchNetflixOriginals: `/discover/tv?api_key=${REACT_API_KEY}&with_networks=213`,
+   fetchTopRated: `/movie/top_rated?api_key=${REACT_API_KEY}&language=en-US`,
+   fetchActionMovies: `/discover/movie?api_key=${REACT_API_KEY}&with_genres=28`,
+   fetchComedyMovies: `/discover/movie?api_key=${REACT_API_KEY}&with_genres=35`,
+   fetchHorrorMovies: `/discover/movie?api_key=${REACT_API_KEY}&with_genres=27`,
+   fetchDocumentariesMovies: `/discover/movie?api_key=${REACT_API_KEY}&with_genres=99`,
+   fetchRomanceMovies: `/discover/movie?api_key=${REACT_API_KEY}&with_genres=10749`
  }
 
  export default requests;
